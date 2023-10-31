47e38454d4255b407e750f993353fa4f75ebc99b
thomas@xza.fr <thomas@xza.fr>
2023-10-31 18:24:02 +0000

    At the point of generating squares with gen_squares().

--- 98.c
+++ 98.c
@@ -21,10 +21,13 @@ void
 print_char_count_quantities(int8 *alpha_char_count);
 
 void
-find_anagrams(int8 *total_char_count, int8 *alpha_char_count);
+find_anagrams(int8 *total_char_count, int8 *alpha_char_count, int *anagram_pairs);
 
 int8
 compare_char_counts(int8 *char_count_a, int8 *char_count_b);
+
+void
+gen_squares(int *squares, int max_n);
 
 void
 main() {
@@ -33,20 +36,33 @@ main() {
 
   int8 alpha_char_count[LINES * 26] = {0};
 
+  int anagram_pairs[128] = {0};
+
+  int squares[0] = {0};
+
   setup_data(&total_char_count[0], &alpha_char_count[0]);
 
-  find_anagrams(&total_char_count[0], &alpha_char_count[0]);
+  find_anagrams(&total_char_count[0], &alpha_char_count[0], &anagram_pairs[0]);
+
+  gen_squares(&squares[0], 362880);
 
 }
 
 void
-find_anagrams(int8 *total_char_count, int8 *alpha_char_count) {
+gen_squares(int *squares, int max_n) {
 
-  int i, j;
+  
+
+}
+
+void
+find_anagrams(int8 *total_char_count, int8 *alpha_char_count, int *anagram_pairs) {
+
+  int i, j, a = 0;
 
   int8 anagram_chk, anagram_pair;
 
-  int8 line_a[16], line_b[16] = {0};
+  /* int8 line_a[16], line_b[16] = {0}; */
 
   for ( i = 0 ; i < LINES - 1 ; i++ ) {
 
@@ -71,21 +87,21 @@ find_anagrams(int8 *total_char_count, int8 *alpha_char_count) {
 
       if ( anagram_pair == 1 ) {
 
-	memset(line_a, 0, sizeof(line_a));
+	*(anagram_pairs + a) = i;
 
-	memset(line_b, 0, sizeof(line_b));
+	*(anagram_pairs + a + 1) = j;
 
-	printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n");
+	a += 2;
 
-	print_char_count_quantities(alpha_char_count + i * 26);
+	/* memset(line_a, 0, sizeof(line_a)); */
 
-	print_char_count_quantities(alpha_char_count + j * 26);
+	/* memset(line_b, 0, sizeof(line_b)); */
 
-	file_line_to_int_8_array(&line_a[0], "98_words.txt", i);	
+	/* file_line_to_int_8_array(&line_a[0], "98_words.txt", i);	 */
 
-	file_line_to_int_8_array(&line_b[0], "98_words.txt", j);
+	/* file_line_to_int_8_array(&line_b[0], "98_words.txt", j); */
 
-	printf("%s %s \n\n", line_a, line_b);
+	/* printf("%s %s \n", line_a, line_b); */
 
       }
       
