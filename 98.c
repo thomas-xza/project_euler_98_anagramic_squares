a29b620c8da5a9d93067cdbc86cb081edecdcadb
thomas@xza.fr <thomas@xza.fr>
2023-10-31 18:04:51 +0000

    Realised the main() refactor introduced a bug in setup_data(). Now fixed

--- 98.c
+++ 98.c
@@ -21,6 +21,12 @@ void
 print_char_count_quantities(int8 *alpha_char_count);
 
 void
+find_anagrams(int8 *total_char_count, int8 *alpha_char_count);
+
+int8
+compare_char_counts(int8 *char_count_a, int8 *char_count_b);
+
+void
 main() {
 
   int8 total_char_count[LINES] = {0};
@@ -28,6 +34,85 @@ main() {
   int8 alpha_char_count[LINES * 26] = {0};
 
   setup_data(&total_char_count[0], &alpha_char_count[0]);
+
+  find_anagrams(&total_char_count[0], &alpha_char_count[0]);
+
+}
+
+void
+find_anagrams(int8 *total_char_count, int8 *alpha_char_count) {
+
+  int i, j;
+
+  int8 anagram_chk, anagram_pair;
+
+  int8 line_a[16], line_b[16] = {0};
+
+  for ( i = 0 ; i < LINES - 1 ; i++ ) {
+
+    for ( j = i + 1 ; j < LINES ; j++ ) {
+
+      anagram_chk = 0;
+
+      anagram_pair = 0;
+
+      if ( *(total_char_count + i) == *(total_char_count + j) ) {
+
+	anagram_chk = 1;
+
+      }
+
+      if ( anagram_chk == 1 ) {
+
+	printf("%d %d\n", *(total_char_count + i), *(total_char_count + j));
+      
+	anagram_pair = \
+	  compare_char_counts(alpha_char_count + i * 26, alpha_char_count + j * 26);
+
+	/* print_char_count_quantities */
+
+      }
+
+      if ( anagram_pair == 1 ) {
+
+	memset(line_a, 0, sizeof(line_a));
+
+	memset(line_b, 0, sizeof(line_b));
+
+	file_line_to_int_8_array(&line_a[0], "98_words.txt", i);	
+
+	file_line_to_int_8_array(&line_b[0], "98_words.txt", j);
+
+	printf("%s %s \n\n", line_a, line_b);
+
+      }
+      
+    }
+
+  }  
+
+}
+
+int8
+compare_char_counts(int8 *char_count_a, int8 *char_count_b) {
+
+  int8 i;
+
+  print_char_count_quantities(char_count_a);
+
+  print_char_count_quantities(char_count_b);
+
+  for ( i = 0 ; i < 16 ; i++ ) {
+
+    if ( *(char_count_a + i) != *(char_count_b + i) ) {
+
+	return 0;
+
+    }
+
+  }
+
+  return 1;
 
 }
 
@@ -42,11 +127,11 @@ setup_data(int8 *total_char_count, int8 *alpha_char_count) {
 
     file_line_to_int_8_array(&line[0], "98_words.txt", line_n);
 
-    char_count(&line[0], (total_char_count), (alpha_char_count + line_n * 26), line_n);
+    char_count(&line[0], total_char_count + line_n, alpha_char_count + line_n * 26, line_n);
 
     print_char_count_quantities((alpha_char_count + line_n * 26));
 
-    printf("  %d  %s \n\n", total_char_count[line_n], line);
+    /* printf("  %d  %s \n\n", total_char_count[line_n], line); */
 
     memset(line, 0, sizeof(line));
 
@@ -59,11 +144,11 @@ print_char_count_quantities(int8 *alpha_char_count) {
 
   int8 i;
 
-  printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n");
+  /* printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n"); */
 
   for ( i = 0 ; i < 26 ; i++ ) {
 
-    printf("%d ", *(alpha_char_count + i));
+    /* printf("%d ", *(alpha_char_count + i)); */
 
   }
 
